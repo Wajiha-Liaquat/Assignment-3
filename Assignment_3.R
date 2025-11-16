9414a9fcf382c0ec6b46b2905c90104427771ed4
Wajiha Liaquat <wajihaliaquat959@gmail.com>
2025-11-16 17:45:50 +0500

    Imported Tables from Sakila database

--- Assignment_3.R
+++ Assignment_3.R
@@ -5,3 +5,12 @@ con <- dbConnect(
   dbname = "sakila",
   host = "localhost"
 )
+
+film <- as.data.table(dbReadTable(con, "film"))
+language <- as.data.table(dbReadTable(con, "language"))
+customer <- as.data.table(dbReadTable(con, "customer"))
+store <- as.data.table(dbReadTable(con, "store"))
+payment <- as.data.table(dbReadTable(con, "payment"))
+staff <- as.data.table(dbReadTable(con, "staff"))
+inventory <- as.data.table(dbReadTable(con, "inventory"))
+rental <- as.data.table(dbReadTable(con, "rental"))
