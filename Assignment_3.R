50ecb00c61f38121436fd68eee6318841775df7d
Wajiha Liaquat <wajihaliaquat959@gmail.com>
2025-11-16 17:44:07 +0500

    Connected to MySQL sakila database

--- Assignment_3.R
+++ Assignment_3.R
@@ -1,7 +1,7 @@
-install.packages("RMySQL")     # For MySQL connection
-install.packages("DBI")        # Database interface
-install.packages("data.table") # For fast data operations
-
-library(DBI)
-library(RMySQL)
-library(data.table)
+con <- dbConnect(
+  RMySQL::MySQL(),
+  user = "root",
+  password = "Samsung7898$",
+  dbname = "sakila",
+  host = "localhost"
+)
