9407eee0c14ed835b2989e0c51b8ab71e4e1ff31
Wajiha Liaquat <wajihaliaquat959@gmail.com>
2025-11-16 17:41:47 +0500

    Installed and loaded packages

---
+++ Assignment_3.R
@@ -0,0 +1,7 @@
+install.packages("RMySQL")     # For MySQL connection
+install.packages("DBI")        # Database interface
+install.packages("data.table") # For fast data operations
+
+library(DBI)
+library(RMySQL)
+library(data.table)
